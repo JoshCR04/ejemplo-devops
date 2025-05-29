91aae66aac4c362dde2cc3699e6a02cae9140806
JoshCR04 <joshuacerdascr@gmail.com>
2025-05-28 19:19:20 -0600

    Prueba 6

--- src/components/card.ts
+++ src/components/card.ts
@@ -26,11 +26,7 @@ export class Card {
     </section>
     `;
 
-        const btn = document.getElementById('joinBtn');
-        const msg = document.getElementById('mensaje');
-        btn?.addEventListener('click', () => {
-            msg?.classList.remove('hidden');
-        });
+  
     }
 
 
